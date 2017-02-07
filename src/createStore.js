cef0ddc35d1c7b08c41cd01ec0056ac357d8876e
cy2003 <catyyee@hotmail.com>
2017-02-07 17:18:13 -0500

    done

--- src/createStore.js
+++ src/createStore.js
@@ -1,10 +1,26 @@
-export default function createStore(reducer){
-
-}
+// export default function createStore(reducer){
+//
+// }
 
 // solution
 export default function createStore(reducer){
+
+  let state;
+
+  function dispatch(action){
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState(){
+    return state;
+  }
+
+  dispatch({type: '@@INIT'})
+
+  return {dispatch: dispatch, getState: getState}
+
 }
 
-function render(){
-}
+  function render(){
+  }
